782a6c7596e4464b4912880e2e7286aa06bf8ccb
Nice Fontaine <wolfhard.fehre@motion-tag.com>
2023-03-24 18:36:42 +0100

    change minor things (#4)

--- tasks.py
+++ tasks.py
@@ -2,10 +2,12 @@ from invoke import task
 
 
 @task(help={'screen': 'the screen position within the defined grid'})
-def client(c, suffix, screen=0):
-    c.run(f'PYTHONPATH=. python client/client.py 192.168.0.{suffix} --screen={screen}')
+def client(c, ip='127.0.0.1', screen=0):
+    """call client with server IP and a screen number"""
+    c.run(f'PYTHONPATH=. python client/client.py {ip} --screen={screen}')       # ip=192.168.0.230
 
 
 @task
 def server(c):
+    """start server"""
     c.run('PYTHONPATH=. python server/server.py')
